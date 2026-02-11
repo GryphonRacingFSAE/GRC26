999425fcc0a6eb8e7cf629dcf775616524fedc12
miguelvillaroman <miguelvillaromans@gmail.com>
2026-02-11 17:14:55 -0500

    Update PeripheralTask.cpp
    Test Lights Program On/Off + Different Colours

--- Dash/src/Tasks/PeripheralTask.cpp
+++ Dash/src/Tasks/PeripheralTask.cpp
@@ -7,7 +7,8 @@
 
 CRGB leds[NUM_LEDS];
 
-void PeripheralTask(void* pvParameters) {
+void PeripheralTask(void *pvParameters)
+{
     Serial.println("[Periph] Task Started");
 
     TickType_t xLastWakeTime = xTaskGetTickCount();
@@ -18,9 +19,22 @@ void PeripheralTask(void* pvParameters) {
     FastLED.setBrightness(80);
     FastLED.setMaxPowerInVoltsAndMilliamps(5, 1500);
 
-    for (;;) {
+    for (;;)
+    {
         vTaskDelayUntil(&xLastWakeTime, xFrequency);
         Serial.println("Peripheral Task");
         // TODO: Blink LEDs, Read Buttons, Update IO Expander
+
+        fill_solid(leds, NUM_LEDS, CRGB::Blue);
+        FastLED.show();
+        vTaskDelay(pdMS_TO_TICKS(100));
+
+        fill_solid(leds, NUM_LEDS, CRGB::Green);
+        FastLED.show();
+        vTaskDelay(pdMS_TO_TICKS(100));
+
+        fill_solid(leds, NUM_LEDS, CRGB::Red);
+        FastLED.show();
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
