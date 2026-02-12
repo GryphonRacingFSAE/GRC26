b0d8d642b9af6ece98acc5965f18c23327e9212f
dtrive02 <dtrive02@uoguelph.ca>
2026-02-12 16:25:57 -0500

    Changes to Task Factory and DataAcqTask
    Made changes to task factory and DataAcqTask.ccp Calls struct from DataAqcTask.h

--- Dash/src/Tasks/DataAcqTask.cpp
+++ Dash/src/Tasks/DataAcqTask.cpp
@@ -1,5 +1,6 @@
 #include <DataAcqTask.h>
 #include <Arduino.h>
+#include "driver/twai.h"
 
 #define DATA_TASK_PERIOD_MS 20
 
@@ -14,8 +15,71 @@ void DataAcqTask(void* pvParameters) {
     for (;;) {
         vTaskDelayUntil(&xLastWakeTime, xFrequency);
         Serial.println("Data Acquisition Task");
-        // TODO: Read CAN data
-        // TODO: Store data in the struct 
-        // MAKE SURE TO USE A SEMAPHORE BEFORE WRITING TO THE STRUCT  
+        twai_message_t rx_msg;
+
+        // Non-blocking receive (since task runs every 20ms)
+        if (twai_receive(&rx_msg, 0) == ESP_OK) {
+
+            switch (rx_msg.identifier) {
+
+            case 0x0A2: {   // Motor Controller Temps
+
+                int16_t hot_spot_temp =
+                    (int16_t)((rx_msg.data[3] << 8) | rx_msg.data[2]);
+
+                int16_t coolant_temp =
+                    (int16_t)((rx_msg.data[1] << 8) | rx_msg.data[0]);
+
+                if (xSemaphoreTake(params->dataMutex, portMAX_DELAY) == pdTRUE) {
+                    params->sharedData->motor_controller_temp = hot_spot_temp;
+                    params->sharedData->coolant_temp = coolant_temp;
+                    xSemaphoreGive(params->dataMutex);
+                }
+
+                break;
+            }
+
+            case 0x0A7: {   // DC Bus Voltage
+
+                int16_t dc_bus_voltage =
+                    (int16_t)((rx_msg.data[1] << 8) | rx_msg.data[0]);
+
+                if (xSemaphoreTake(params->dataMutex, portMAX_DELAY) == pdTRUE) {
+                    params->sharedData->tractive_voltage = dc_bus_voltage;
+                    xSemaphoreGive(params->dataMutex);
+                }
+
+                break;
+            }
+
+            case 0x0B0: {   // Motor Speed
+
+                int16_t motor_speed =
+                    (int16_t)((rx_msg.data[3] << 8) | rx_msg.data[2]);
+
+                if (xSemaphoreTake(params->dataMutex, portMAX_DELAY) == pdTRUE) {
+                    params->sharedData->motor_speed = motor_speed;
+                    xSemaphoreGive(params->dataMutex);
+                }
+
+                break;
+            }
+
+            case 0x0E0: {   // BMS Max Temp
+
+                int8_t max_temp = (int8_t)rx_msg.data[0];
+
+                if (xSemaphoreTake(params->dataMutex, portMAX_DELAY) == pdTRUE) {
+                    params->sharedData->bms_max_temp = max_temp;
+                    xSemaphoreGive(params->dataMutex);
+                }
+
+                break;
+            }
+
+            default:
+                break;
+            }
+        }
     }
 }
--- Dash/src/Tasks/TaskFactory.cpp
+++ Dash/src/Tasks/TaskFactory.cpp
@@ -3,25 +3,48 @@
 #include <DataAcqTask.h>
 #include <PeripheralTask.h>
 #include <Arduino.h>
+#include <DashData.h>
+#include "driver/twai.h"
 
 // Static Handles (Private)
 static SemaphoreHandle_t guiMutexHandle;
 static QueueHandle_t     dataQueueHandle;
+static SemaphoreHandle_t dataMutexHandle;
 
 static TaskHandle_t guiTaskHandle = NULL;
 static TaskHandle_t dataTaskHandle = NULL;
 static TaskHandle_t periphTaskHandle = NULL;
 
 // Static Parameters
+static DashData sharedData;
 static GuiTaskParameters      guiParams;
 static DataAcqTaskParameters  dataParams;
 static PeripheralTaskParameters periphParams;
 
 void createTasks() {
     Serial.println("[Factory] Creating Tasks...");
+    // 0. CAN Init
+twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(
+    GPIO_NUM_5,   // TX pin (change if needed)
+    GPIO_NUM_4,   // RX pin (change if needed)
+    TWAI_MODE_NORMAL
+);
+
+twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
+    Serial.println("[Factory] TWAI Driver Installed");
+}
+
+if (twai_start() == ESP_OK) {
+    Serial.println("[Factory] TWAI Started");
+}
+    
 
     // 1. Objects
     guiMutexHandle = xSemaphoreCreateMutex();
+    dataMutexHandle = xSemaphoreCreateMutex();
     dataQueueHandle = xQueueCreate(10, sizeof(int)); // Update sizeof() later
 
     // 2. Params
@@ -29,6 +52,8 @@ void createTasks() {
     guiParams.dataQueue = &dataQueueHandle;
     
     dataParams.dataQueue = &dataQueueHandle;
+    dataParams.dataMutex = dataMutexHandle;
+    dataParams.sharedData = &sharedData;
     
     periphParams.guiMutex = &guiMutexHandle;
 
