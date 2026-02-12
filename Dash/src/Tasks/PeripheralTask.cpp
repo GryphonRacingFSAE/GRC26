dd54cfe8cd89875a93ee742aa506d0c7ed39000a
miguelvillaroman <miguelvillaromans@gmail.com>
2026-02-12 15:09:51 -0500

    Test Mess 1
    different codes used to test the LEDS

--- Dash/src/Tasks/PeripheralTask.cpp
+++ Dash/src/Tasks/PeripheralTask.cpp
@@ -18,6 +18,7 @@ void PeripheralTask(void *pvParameters)
     FastLED.addLeds<LED_TYPE, LED_DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
     FastLED.setBrightness(80);
     FastLED.setMaxPowerInVoltsAndMilliamps(5, 1500);
+    digitalWrite(44, HIGH); // Power on the LEDs
 
     for (;;)
     {
@@ -25,16 +26,27 @@ void PeripheralTask(void *pvParameters)
         Serial.println("Peripheral Task");
         // TODO: Blink LEDs, Read Buttons, Update IO Expander
 
-        fill_solid(leds, NUM_LEDS, CRGB::Blue);
-        FastLED.show();
-        vTaskDelay(pdMS_TO_TICKS(100));
+        // fill_solid(leds, NUM_LEDS, CRGB::Blue);
+        // FastLED.show();
+        // printf("LEDs set to Blue\n");
+        // // vTaskDelay(pdMS_TO_TICKS(100));
 
-        fill_solid(leds, NUM_LEDS, CRGB::Green);
-        FastLED.show();
-        vTaskDelay(pdMS_TO_TICKS(100));
-
+        for (int i = 0; i < NUM_LEDS; i++)
+        {
+            leds[i] = CRGB(255, 0, 0); // Red
+        }
         fill_solid(leds, NUM_LEDS, CRGB::Red);
         FastLED.show();
-        vTaskDelay(pdMS_TO_TICKS(100));
+        printf("LEDs set to Red\n");
+
+        // fill_solid(leds, NUM_LEDS, CRGB::Green);
+        // FastLED.show();
+        // printf("LEDs set to Green\n");
+        // vTaskDelay(pdMS_TO_TICKS(100));
+
+        // fill_solid(leds, NUM_LEDS, CRGB::Red);
+        // FastLED.show();
+        // printf("LEDs set to Red\n");
+        // vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
